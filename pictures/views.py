039362120f9f0e40570b467a9e13fc4e415298ed
GasaGentille <gisagentille55@gmail.com>
2019-10-17 18:37:34 +0200

    Creating image&profile function

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Image,Profile 
+
 
 # Create your views here.
+
+def image(request):
+    images = Image.objects.all()
+    print(images)
+    return  render (request,'index.html',{"images":images})
+
+def profile(request):
+    return  render (request,'profile.html')
+
+    
