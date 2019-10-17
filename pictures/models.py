f964f29f00a28cdeaf03d61afead9192809b49b4
GasaGentille <gisagentille55@gmail.com>
2019-10-17 18:09:46 +0200

    Creating Image and profile modals

---
+++ pictures/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to = 'profile/', null=True)
+    bio = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.location
+        
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        location = Location.objects.filter_by(id = 2).update(location = 'kigali')
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/', null=True)
+    image_name = models.CharField(max_length =30)
+    image_caption = models.CharField(max_length =30)
+    likes = models.IntegerField(default=0)
+    comments = models.CharField(max_length =30)
+    profile_photo =  models.ForeignKey(Profile, null=True)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        images = Image.objects.filter_by(id = 2).update(name = 'people')
+
+
+
+    
+
